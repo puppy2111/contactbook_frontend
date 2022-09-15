982321f3b9c5b4b157f4716d44d8cd8dc5d5f6a9
puppy2111 <huynhdaihuybank3@gmail.com>
2022-09-15 22:11:53 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -33,15 +33,25 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span
+          >
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 <script>
-import ContactCard from "../components/ContactCard.vue";
-import InputSearch from "../components/InputSearch.vue";
-import ContactList from "../components/ContactList.vue";
-import ContactService from "../services/contact.service";
+import ContactCard from "@/components/ContactCard.vue";
+import InputSearch from "@/components/InputSearch.vue";
+import ContactList from "@/components/ContactList.vue";
+import ContactService from "@/services/contact.service";
 export default {
   components: {
     ContactCard,
